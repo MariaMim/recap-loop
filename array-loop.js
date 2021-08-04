865aaaabee02f7c9d27222c09b1b21f380098385
MariaMim <mariamim266@gmail.com>
2021-08-04 16:08:17 +0600

    first commit

---
+++ array-loop.js
@@ -0,0 +1,11 @@
+var numbers = [45, 86, 87, 12, 98, 82, 78, 45,23];
+for(var i = 0; i< numbers.length; i++){
+    var element = numbers[i]
+    console.log(element);
+}
+
+
+// for(var i = 0; i < items.length; i++ ){
+//     var item = items[i];
+//     console.log(item);
+// }
---
+++ for-loop.js
@@ -0,0 +1,22 @@
+// var roastGiven = 0;
+// while(roastGiven < 7){
+//     console.log('Roast den, please!');
+//     roastGiven++;
+// }
+
+
+// console.log('running a for loop');
+// for(var roastGiven = 0; roastGiven < 7; roastGiven++){
+//     console.log('Roast den, please!!')
+//     console.log(roastGiven);
+// }
+
+// for loop er jnno variable name short kora jay
+// for (var i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+//odd
+for(var i = 1; i <=20; i+=2) {
+    console.log(i);
+}
